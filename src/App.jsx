720eb7cead625bdeb0fe0bbb93a8d974d30bae9a
Alireza Shabani <alireza4o4shabani@gmail.com>
2026-09-18 13:31:13 +0330

    add smooth scrolling behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useLayoutEffect } from "react";
-import { useTranslation } from "react-i18next";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import SelectedWork from "./components/SelectedWork/SelectedWork";
 import MoreProjects from "./components/MoreProjects/MoreProjects";
-
-import "./App.css";
 import HowIBuild from "./components/HowIBuild/HowIBuild";
 import SkillsAndStack from "./components/SkillsAndStack/SkillsAndStack";
 import Contact from "./components/Contact/Contact";
 
-function App() {
-  const { i18n } = useTranslation();
+import "./App.css";
 
-  useLayoutEffect(() => {
-    document.documentElement.lang = i18n.language;
-    document.documentElement.dir = i18n.language === "fa" ? "rtl" : "ltr";
-  }, [i18n]);
+function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 2.5,
+      smoothWheel: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => lenis.destroy();
+  }, []);
 
   return (
     <div className="w-full min-h-screen overflow-x-hidden bg-[#0b0e0c]">
